b5de1bf8253b7af3187707aec3dad2e716f896d0
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-06-19 15:01:02 +0200

    Aufgabe

---
+++ src/main/java/de/lubowiecki/uebung/U4Objektbeziehungen.java
@@ -0,0 +1,18 @@
+package de.lubowiecki.uebung;
+
+public class U4Objektbeziehungen {
+
+    // Schreibe eine Klasse, die einen Fußbalspieler darstellt
+    // Er hat einen Vornamen und Nachnamen, sowie eine Nummer und Spezialisierung.
+    // Schreibe passende Getter/Setter und Konstruktoren
+
+    // Schreibe eine Klasse, die ein Team darstellt
+    // Neben einem Namen oder einer Bezeichnung soll es die Möglichkeit
+    // geben, Spieler diesem Team zuzuweisen und die Zusammenstellung des Teams anzuzeigen
+
+    // Schreibe eine Klasse, die einen Ball darstellt
+    // Realisiere durch passende Methoden, dass ein Spieler den Ball annehmen kann
+    // und diesen an einen anderen Spieler weitergeben kann
+
+
+}
