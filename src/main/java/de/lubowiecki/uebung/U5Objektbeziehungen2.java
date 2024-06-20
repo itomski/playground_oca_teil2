80ba122dbcb97ed4dacf8caa6c824f199236a6f7
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-06-20 15:18:40 +0200

    Aufgabe 4

---
+++ src/main/java/de/lubowiecki/uebung/U5Objektbeziehungen2.java
@@ -0,0 +1,25 @@
+package de.lubowiecki.uebung;
+
+public class U5Objektbeziehungen2 {
+
+    // Schreibe eine Klasse für einen Roboter
+    // Neben einer Bezeichnung kann der Roboter aus verschiedenen Teilen zusammengestzt werden
+    // Dazu gehören Hauptfunktion, Fortbewegung, Kommunikation, Energieversorgung
+    // Diese sollen als Objekte dem Roboter zugeordnet werden können.
+
+    // Z.B. Roboter mit dem Namen Alpha123
+    // Haptpunktionen: Küchenhelfer
+    // Fortbewegung: Raupenatrieb
+    // Kommunikation: Deutsch, einfach
+    // Energieversorgung: Solar
+
+    // Die Module aus denen der Roboter zusammengebaut ist sollen lediglich einfache Meldungen
+    // auf der Konsole machen
+
+    // z.B. alpha123.hauptfunktion1(); // Gibt aus: Zutaten werden zerkleinert
+    // z.B. alpha123.hauptfunktion2(); // Gibt aus: Zutaten werden gekocht
+    // z.B. alpha123.bewegung(); // Gibt aus: Raupt sich vorwärts
+    // z.B. alpha123.komminikation(); // Gibt aus: Hallo. Mein Name ist Alpha123
+    // z.B. alpha123.aufladen(); // Gibt aus: Nicht nötig. Ich arbeite Solar
+
+}
