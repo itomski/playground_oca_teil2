1b713db3162d103d9c235b3870a6ccbe14c8ce82
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-06-21 11:26:37 +0200

    Zahlen und Datentypen

--- src/main/java/de/lubowiecki/uebung/U4IsA.java
+++ src/main/java/de/lubowiecki/uebung/U4IsA.java
@@ -1,5 +1,8 @@
 package de.lubowiecki.uebung;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class U4IsA {
 
     // Schreibe eine Klasse, die ein Zoo realisiert
@@ -7,4 +10,55 @@ public class U4IsA {
     // Schaffe die Möglichkeit, dass verschidene Tierobjekte dem Zoo zugeordnet werden können
     // Es kann angezeigt werden, welche Tiere dem Zoo angehören
 
+    public static void main(String[] args) {
+        Tier t1 = new Tier("Leo");
+        t1.vorstellen();
+        t1.umbennen("Hansen");
+        t1.vorstellen();
+
+        System.out.println();
+
+        List<String> namen = new ArrayList<>();
+        namen.add("Peter");
+        namen.add("Bruce");
+        namen.add("Carol");
+        System.out.println(namen);
+
+        System.out.println();
+
+        List<Tier> tiere = new ArrayList<>();
+        tiere.add(t1);
+        tiere.add(new Tier("Klaus"));
+        Tier t2 = new Tier("Peters");
+        tiere.add(t2);
+
+        System.out.println(tiere);
+
+        System.out.println(t2); // toString wird automatisch aufgerufen
+
+
+    }
+}
+
+class Tier {
+
+    private String name;
+
+    public Tier(String name) {
+        this.name = name;
+    }
+
+    public void vorstellen() {
+        System.out.println("Ich bin " + name);
+    }
+
+    public void umbennen(String name) {
+        this.name = name;
+    }
+
+    // To String konvertiert ein Objekt in einen String
+    @Override
+    public String toString() {
+        return "####:" + name;
+    }
 }
