e8b865f506f9443ca6c2bfe3a17257cba7bc66b3
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-06-24 14:02:24 +0200

    Aufgabe

--- src/main/java/de/lubowiecki/StringManipulationTest.java
+++ src/main/java/de/lubowiecki/StringManipulationTest.java
@@ -1,6 +1,7 @@
 package de.lubowiecki;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class StringManipulationTest {
 
@@ -10,15 +11,30 @@ public class StringManipulationTest {
 
         // Soll die Änderung permanent sein, muss die Referenz neu zugewiesen werden
         wort = wort.toUpperCase(); // In Großbuchstaben ändern
+
+        // Textteile auslesen
         //wort = wort.substring(2); // Liefert alles ab Index 2
         wort = wort.substring(1, 5); // Liefert alles ab Index 2 bis Index 5
 
         System.out.println(wort);
+
+        // Muster überprüfen
         //System.out.println(wort.matches("[a-zA-Z]{5,10}"));
         String regexp = "\\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}\\b";
         String email = "peter.parker@shield.org";
         System.out.println("Email: " + email.matches(regexp));
 
+        // Sätze und Wörter zerlegen
+        wort = "Das ist das Haus von Nikigraus";
+        String[] teile = wort.split(" ");
+        System.out.println(Arrays.asList(teile));
+
+        // Inhalt prüfen
+        System.out.println("Beginnt mit D: " + wort.startsWith("D"));
+        System.out.println("Erstes zeichen? : " + wort.charAt(0));
+        System.out.println("Position von H? : " + wort.indexOf("H"));
+
+        // wort = wort.replace("Haus", "Boot");
 
         //machWas(wort); // Kein Literal
         //machWas("Wasser"); // Literal
---
+++ src/main/java/de/lubowiecki/uebung/U7Strings.java
@@ -0,0 +1,10 @@
+package de.lubowiecki.uebung;
+
+public class U7Strings {
+
+    // Schreibe eine Anwendung die vom User einen (längeren) Text entgegen nicht.
+    // Danach kann er ein Wort oder Buchstaben eingeben und bekommt die Positon ausgegeben.
+    // Wenn es mit Text vorkommt, soll danach der gleiche Text ausgegeben werden, allerding das
+    // gesuchte Wort soll duch das gleiche Wort in Großbuchstaben ersetzt werden
+
+}
