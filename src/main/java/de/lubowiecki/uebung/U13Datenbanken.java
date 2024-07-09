e9117764269aed8b72eb1dc22f98b4dc266c27ee
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-07-09 13:22:19 +0200

    Exceptions & JDBC

---
+++ src/main/java/de/lubowiecki/uebung/U13Datenbanken.java
@@ -0,0 +1,12 @@
+package de.lubowiecki.uebung;
+
+public class U13Datenbanken {
+
+    /*
+    Schreibe ein Programm, welches Termine in einer Datenbank speicher kann.
+    Dafür wird ein Datum und ein Titel abgefragt und gespeichert.
+    Danach kann man sich alle Termine in der DB anzeigen lassen.
+    // Als Datum kann auch erstmal ein String benutzt werden (ISO: yyyy-MM-dd)
+    */
+
+}
