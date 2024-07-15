4a5ddfcf80c4df2655b68997642a9793447c1c18
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-07-15 13:10:07 +0200

    Aufgabe

---
+++ src/main/java/de/lubowiecki/uebung/U15Interfaces.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.uebung;
+
+public class U15Interfaces {
+
+    // Schreibe eine Klasse Steckdose mit der Methode stomAn()
+    // Über eine andere Methode der Steckdose anschliessen(...)
+    // kann eine kompatibiles Objekt das ein Gerät darstellt übergeben werden
+    // Schaffe über ein Interface Stecker die Möglichkeit, dass verschiedene Geräte mit der
+    // Steckdose kompatibel sind
+    // Wird die Methode stromAn() der Steckdose aufgerufen, soll das angeschloßene Gerät
+    // aktiv werden... z.B. mit einer passenden Meldung
+
+}
